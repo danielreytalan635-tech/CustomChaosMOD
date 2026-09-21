22b69fe9fba827427be038e71fb2c8a37f91fe68
DRT Talan <danielrey.talan635@gmail.com>
2026-09-21 15:02:32 +0800

    Add Fabric 26.2 Chaos Control keybind

--- src/client/java/me/yourname/customchaos/CustomChaosClient.java
+++ src/client/java/me/yourname/customchaos/CustomChaosClient.java
@@ -1,10 +1,33 @@
 package me.yourname.customchaos;
 
+import com.mojang.blaze3d.platform.InputConstants;
 import net.fabricmc.api.ClientModInitializer;
+import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
+import net.fabricmc.fabric.api.client.keybinding.v1.KeyBindingHelper;
+import net.minecraft.client.KeyMapping;
+import net.minecraft.client.Minecraft;
+import org.lwjgl.glfw.GLFW;
 
 public final class CustomChaosClient implements ClientModInitializer {
+    private static final KeyMapping OPEN_MENU = KeyBindingHelper.registerKeyBinding(
+        new KeyMapping(
+            "key.customchaos.open_menu",
+            InputConstants.Type.KEYSYM,
+            GLFW.GLFW_KEY_K,
+            KeyMapping.Category.register(CustomChaos.id("controls"))
+        )
+    );
+
     @Override
     public void onInitializeClient() {
-        // Client GUI/keybind registration will live here.
+        ClientTickEvents.END_CLIENT_TICK.register(client -> {
+            while (OPEN_MENU.consumeClick()) {
+                if (client.player != null) {
+                    Minecraft.getInstance().gui.setScreen(
+                        new ChaosScreen(Minecraft.getInstance().gui.screen())
+                    );
+                }
+            }
+        });
     }
 }
