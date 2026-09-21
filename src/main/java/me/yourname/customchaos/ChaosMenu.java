d65fc003a160e1010d31cc9f65d15daa4a660040
DRT Talan <danielrey.talan635@gmail.com>
2026-09-21 15:15:11 +0800

    Correct SimpleMenuProvider package for Minecraft 26.2

--- src/main/java/me/yourname/customchaos/ChaosMenu.java
+++ src/main/java/me/yourname/customchaos/ChaosMenu.java
@@ -4,7 +4,7 @@ import net.minecraft.core.component.DataComponents;
 import net.minecraft.network.chat.Component;
 import net.minecraft.server.level.ServerPlayer;
 import net.minecraft.world.SimpleContainer;
-import net.minecraft.world.inventory.SimpleMenuProvider;
+import net.minecraft.world.SimpleMenuProvider;
 import net.minecraft.world.entity.player.Inventory;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.inventory.ChestMenu;
