eca18b5a266c04f03cb50a41e7eb2a95c26297a8
DRT Talan <danielrey.talan635@gmail.com>
2026-09-21 15:01:08 +0800

    Add client initializer to Fabric client source set

---
+++ src/client/java/me/yourname/customchaos/CustomChaosClient.java
@@ -0,0 +1,10 @@
+package me.yourname.customchaos;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public final class CustomChaosClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+        // Client GUI/keybind registration will live here.
+    }
+}
