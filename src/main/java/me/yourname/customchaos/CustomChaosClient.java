cedffaba1f268a4b74ec47dced1d082d35d0a8a7
DRT Talan <danielrey.talan635@gmail.com>
2026-09-21 14:58:34 +0800

    Add Fabric client entrypoint

---
+++ src/main/java/me/yourname/customchaos/CustomChaosClient.java
@@ -0,0 +1,10 @@
+package me.yourname.customchaos;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public final class CustomChaosClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+        // Client entrypoint reserved for the Chaos Control GUI/keybind.
+    }
+}
